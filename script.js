057c12ba15c343aed9dc94e741f01594072022c7
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-29 17:48:24 +0100

    Setup skeleton of the wp.blocks.registerBlockType() function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// This wp Objects is a global Object added by WordPress automatically to interact with it.
 
+// namespace/name so there is no conflicts
+wp.blocks.registerBlockType('namespace/name', {
+    title: x,
+    icon: x,
+    category: x,
+    attributes: x,
+    edit: x,
+    save: x
+})
