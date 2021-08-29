14035874e8fed21f10d5c931535d5c31116619c2
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-29 17:33:38 +0100

    Added Decription & Requires at least properties to plugin header

--- plugin.php
+++ plugin.php
@@ -1,9 +1,11 @@
 <?php
     /*
      * Plugin Name: WordPress Blocks
+     * Description: A simple WordPress Plugin to creata a simple custom Gutenberg Block.
      * Author: Ayo Reis
      * Author URI: //ayoreis.com/
      * Version 1.0.0
+     * Requires at least: 5.0.0
      */
 
     function my_enqueue_scripts() {
