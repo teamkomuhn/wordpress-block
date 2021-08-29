3ee516b6072c180ac9216888a63a055eb2c3f099
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-29 17:26:16 +0100

    Adding the wp_enqueue_script() function for script.js

--- plugin.php
+++ plugin.php
@@ -5,4 +5,10 @@
      * Author URI: //ayoreis.com/
      * Version 1.0.0
      */
+
+    function my_enqueue_scripts() {
+        wp_enqueue_script('script.js',)
+    }
+
+    add_action('wp_enqueue_scripts', 'my_enqueue_scripts')
 ?>
