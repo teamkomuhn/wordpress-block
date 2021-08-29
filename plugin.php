01236a342f4d2d19b217a16273744f3b0b43e90e
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-29 17:28:16 +0100

    Added commas

--- plugin.php
+++ plugin.php
@@ -7,8 +7,9 @@
      */
 
     function my_enqueue_scripts() {
-        wp_enqueue_script('script.js',)
+        // See the meaning of each attribute of the wp_enqueue_script() function here: https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+        wp_enqueue_script('script.js', plugin_dir_url(__FILE__) . 'script.js', array('wp-blocks', 'wp-i18n', 'wp-editor'), true);
     }
 
-    add_action('wp_enqueue_scripts', 'my_enqueue_scripts')
+    add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
 ?>
