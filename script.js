cedbd1e9d353c123a62eb02656d61a463d1dcfad
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-29 18:06:20 +0100

    Working?

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+'use strict'
+
 // This wp Objects is a global Object added by WordPress automatically to interact with it.
 
-// namespace/name so there is no conflicts
+// namespace/name is the computer redable name and title is the human redable name
 wp.blocks.registerBlockType('namespace/name', {
-    title: x,
-    icon: x,
-    category: x,
-    attributes: x,
-    edit: x,
-    save: x
+    title: 'Super cool name',
+    icon: 'smiley', // Use Dashicon icon name: https://developer.wordpress.org/resource/dashicons/
+    category: 'common', // Block category
+    attributes: {
+        title: {type: 'string'},
+        content: {type: 'string'} // I think it should use the class instead of a sting Eg:. 'string' => String
+    },
+    edit: (properties) => {
+        return wp.element.createElement(
+            "div",
+            null,
+            wp.element.createElement(
+                "input",
+                null
+            )
+        )
+    },
+    save: (properties) => {
+        return null
+    }
 })
+
+//wp.element = React reatch may work now but not in the future
