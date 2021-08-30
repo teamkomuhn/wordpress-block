43ec1a13c059e6f5aa070bb9cf7c035072d6d861
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-30 16:40:54 +0100

    🆔 Register block type the rigth way

--- plugin.php
+++ plugin.php
@@ -8,10 +8,14 @@
      * Requires at least: 5.0.0
      */
 
-    function my_enqueue_scripts() {
+    function my_register_block_types() {
         // See the meaning of each attribute of the wp_enqueue_script() function here: https://developer.wordpress.org/reference/functions/wp_enqueue_script/
         wp_enqueue_script('script.js', plugin_dir_url(__FILE__) . 'script.js', array('wp-blocks', 'wp-i18n', 'wp-editor'), true);
+
+        register_block_type('namespace/name', array(
+            'editor_script' => 'script.js'
+        ))
     }
 
-    add_action('enqueue_block_editor_assets', 'my_enqueue_scripts');
+    add_action('init', 'my_register_block_types');
 ?>
